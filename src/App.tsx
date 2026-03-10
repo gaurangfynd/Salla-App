5512b2033155c849006308d3e21edd003027b46b
Gaurang Keluskar <gaurangkeluskar@fynd-external.com>
2026-03-10 05:10:05 +0000

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import AIAgent from './components/aiAgent'
-import './common/styles/main.less'
+import AIAgent from "./components/aiAgent";
+import AIAgentSetup from "./components/aiAgent/setup";
+import "./common/styles/main.less";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/theme-context";
 
 function App() {
-
   return (
-    <>
-      <AIAgent />
-    </>
-  )
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AIAgent />} />
+          <Route path="/setup" element={<AIAgentSetup />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
