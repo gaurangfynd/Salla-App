f19592c5d1b3ac84a9a24c31b392ea656086baac
Gaurang Keluskar <gaurangkeluskar@fynd-external.com>
2026-03-17 13:03:51 +0530

    made changes

--- src/components/aiAgent/index.tsx
+++ src/components/aiAgent/index.tsx
@@ -939,7 +939,7 @@ function AIAgent() {
                       24/7 support, personalized shopping experiences, and more
                     </p>
                   </div>
-                  {ableToCreateBot && (
+                  {/* {ableToCreateBot && ( */}
                     <button
                       type="button"
                       className="cursor-pointer ai-agent__body__card__header-button inline-flex items-center justify-center rounded-lg bg-[var(--salla-secondary-color)] px-4 py-2 text-sm font-medium text-[var(--salla-light-mode-primary-color)] shadow-sm hover:bg-gray-800 transition"
@@ -948,7 +948,7 @@ function AIAgent() {
                     >
                       Setup in 1 click
                     </button>
-                  )}
+                  {/* )} */}
                 </div>
                 <div className="ai-agent__body__card__header-right">
                   <SVGLoader
